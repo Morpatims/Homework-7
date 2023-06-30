e9a181f5a80f7ca346b4eba129a87634bb4bbba3
morpatims <kverteletski@mail.ru>
2023-06-30 15:53:16 +0300

    Add tasks 47, 50, 52.

---
+++ Task 47/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Enter the number of rows of the array: ");
+int rows = Convert.ToInt32(ReadLine());
+
+Write("Enter the number of columns of the array: ");
+int columns = Convert.ToInt32(ReadLine());
+
+double[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * (maxValue - minValue + 1) + minValue;
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]:f2} ");
+        }
+        WriteLine();
+    }
+}
---
+++ Task 52/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Enter the number of rows of the array: ");
+int rows = Convert.ToInt32(ReadLine());
+
+Write("Enter the number of columns of the array: ");
+int columns = Convert.ToInt32(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+double[] AverageValueColumns = FindMeanElements(array);
+WriteLine($"The arithmetic mean of each column = {String.Join("; ", AverageValueColumns)}");
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+double[] FindMeanElements(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[j] += array[i, j] / array.GetLength(0);
+        }
+    }
+    return result;
+}
